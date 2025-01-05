8c1aa4bd02ee7dc05e06f7909d3ee344d9602d3a
jka <bandhujkar123@gmail.com>
2025-01-05 23:59:01 +0530

    05-01-25

---
+++ Learn_Basic_Recursion/src/com/jk/dsa/PrintOnetoNusingRecursion.java
@@ -0,0 +1,13 @@
+package com.jk.dsa;
+
+public class PrintOnetoNusingRecursion {
+
+	public static void main(String[] args) {
+printOnetoNusingRecursion(1,10);
+	}
+public static void printOnetoNusingRecursion(int i,int n) {
+	if(i>n) return;
+	System.out.println(i);
+	printOnetoNusingRecursion((i+1),n);
+}
+}
