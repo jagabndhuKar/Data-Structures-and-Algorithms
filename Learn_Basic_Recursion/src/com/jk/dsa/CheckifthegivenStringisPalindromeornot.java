e7329bbb0d7af26359752a016659fded6f46eaa5
jka <bandhujkar123@gmail.com>
2025-01-09 02:41:32 +0530

    09-01-2025

--- Learn_Basic_Recursion/src/com/jk/dsa/CheckifthegivenStringisPalindromeornot.java
+++ Learn_Basic_Recursion/src/com/jk/dsa/CheckifthegivenStringisPalindromeornot.java
@@ -3,8 +3,33 @@ package com.jk.dsa;
 public class CheckifthegivenStringisPalindromeornot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(checkifthegivenStringisPalindromeornot("aba"));
+		System.err.println(checkifthegivenStringisPalindromeornot2("abba", 0, 3));
 	}
 
+	public static boolean checkifthegivenStringisPalindromeornot(String s) {
+		int i = 0, j = s.length() - 1;
+		while (i <= j) {
+			if (s.charAt(i) != s.charAt(j))
+				return false;
+			i++;
+			j--;
+		}
+		return true;
+	}
+	
+	public static boolean checkifthegivenStringisPalindromeornot2(String s,int left,int right) {
+	
+		if(left>right) {
+			return true;
+		}else {
+			if (s.charAt(left) != s.charAt(right))
+				return false;
+			left=left+1;
+			right=right-1;
+			System.err.println(left+" "+right);
+			return checkifthegivenStringisPalindromeornot2(s,left,right);
+		}
+	
+	} 
 }
--- Learn_Basic_Recursion/src/com/jk/dsa/ReverseagivenArray.java
+++ Learn_Basic_Recursion/src/com/jk/dsa/ReverseagivenArray.java
@@ -3,31 +3,33 @@ package com.jk.dsa;
 public class ReverseagivenArray {
 
 	public static void main(String[] args) {
-int arr[]= {1,2,3,4,5};
-		reverseagivenArray2(arr,0,arr.length-1);
-	for(int i:arr) {
-		System.out.println(i);
+		int arr[] = { 1, 2, 3, 4, 5 };
+		reverseagivenArray2(arr, 0, arr.length - 1);
+		for (int i : arr) {
+			System.out.println(i);
+		}
 	}
-	}
-public static void reverseagivenArray(int arr[]) {
-	int i=0,j=arr.length-1;
-	while(i<=j) {
-		int temp =arr[i];
-		arr[i]=arr[j];
-		arr[j]=temp;
-		i++;j--;
-	}
-}
 
-public static void reverseagivenArray2(int arr[],int left,int right) {
-if(left<=right) {
-		int temp =arr[left];
-		arr[left]=arr[right];
-		arr[right]=temp;
-		reverseagivenArray2(arr, (left+1), (right-1));
-}else {
-	return;
-}
-}
+	public static void reverseagivenArray(int arr[]) {
+		int i = 0, j = arr.length - 1;
+		while (i <= j) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
+	}
+
+	public static void reverseagivenArray2(int arr[], int left, int right) {
+		if (left <= right) {
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			reverseagivenArray2(arr, (left + 1), (right - 1));
+		} else {
+			return;
+		}
+	}
 
 }
